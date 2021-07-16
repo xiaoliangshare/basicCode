b0a43505ea6dad1ed8a199e5942ec1a4f3497796
lianghaohong <lianghaohong@meituan.com>
2021-07-16 19:48:15 +0800

    Optional用法

--- Demo/src/main/java/OptionalDemo.java
+++ Demo/src/main/java/OptionalDemo.java
@@ -15,19 +15,18 @@ public class OptionalDemo {
      * orElse()方法与orElseGet()方法类似，但是在持有null的时候返回传入的默认值。
      */
     public static void main(String[] args) {
-        Optional< String > fullName = Optional.ofNullable( null );
-        System.out.println( "Full Name is set? " + fullName.isPresent() );
-        System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) );
+        Optional<String> fullName = Optional.ofNullable(null);
+        System.out.println("Full Name is set? " + fullName.isPresent());
+        System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
         System.out.println("Full Name: " + fullName.orElse("[none]"));
-        System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+        System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
         System.out.println("======");
 
 
-
-        Optional< String > firstName = Optional.of( "Tom" );
-        System.out.println( "First Name is set? " + firstName.isPresent() );
-        System.out.println( "First Name: " + firstName.orElseGet( () -> "[none]" ) );
-        System.out.println( firstName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+        Optional<String> firstName = Optional.of("Tom");
+        System.out.println("First Name is set? " + firstName.isPresent());
+        System.out.println("First Name: " + firstName.orElseGet(() -> "[none]"));
+        System.out.println(firstName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
         System.out.println();
     }
 }
