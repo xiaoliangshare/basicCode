cd24ad75f0d262960a000049952dd9a88e4f76e2
lianghaohong <lianghaohong@meituan.com>
2021-11-03 20:13:51 +0800

    xx

--- Xml/src/main/java/XmlDemo.java
+++ Xml/src/main/java/XmlDemo.java
@@ -21,7 +21,7 @@ public class XmlDemo {
         //2、将该文档加载进内存，获取dom树
         Document document = Jsoup.parse(new File(path), "utf-8");
 
-        //3、根据标签名称获取元素对象 Elements对象, Elements extends ArrayList<Element>，就把它看作是一个List<Element>的集合
+        //3、根据标签名称获取元素对象 Elements对象, Elements extends ArrayList<Element>，可以把它看作是一个List<Element>的集合来使用
         Elements names = document.getElementsByTag("name");
         System.out.println(names.size()); // 有2个
 
@@ -29,6 +29,11 @@ public class XmlDemo {
         System.out.println(names.get(0).text());
         Element name1 = names.get(1);
         System.out.println(name1.text());
+        Elements name = name1.getElementsByTag("name"); //Element同样有这个方法
+        System.out.println("---------------------------------");
+        String id = name.get(0).attr("id");
+        System.out.println(id+"---");
+        System.out.println(name);
 
         System.out.println(names);
         System.out.println(document.getElementById("id"));
