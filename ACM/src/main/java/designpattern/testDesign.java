57e8ba1ff737d15b686cd4e5e7acc02f47c39ef9
lianghaohong <lianghaohong@meituan.com>
2022-06-07 21:50:22 +0800

    xx

--- ACM/src/main/java/designpattern/testDesign.java
+++ ACM/src/main/java/designpattern/testDesign.java
@@ -8,22 +8,33 @@ package designpattern;
  * Copyright (C) 2021 Meituan All rights reserved
  */
 public class testDesign {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("main线程开始,用户注册");
         new Thread(() -> {
-            System.out.println(SignalDesign3.getSignalDesign3());
+            for (int i = 0; i < 1000; i++) {
+                System.out.println(i);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }).start();
-        new Thread(() -> {
-            System.out.println(SignalDesign3.getSignalDesign3());
-        }).start();
-        new Thread(() -> {
-            System.out.println(SignalDesign3.getSignalDesign3());
-        }).start();
-        new Thread(() -> {
-            System.out.println(SignalDesign3.getSignalDesign3());
-        }).start();
-        new Thread(() -> {
-            System.out.println(SignalDesign3.getSignalDesign3());
-        }).start();
+        Thread.sleep(100);
+        System.out.println("main线程结束");
+
     }
 }
+// new Thread(() -> {
+//         System.out.println(SignalDesign3.getSignalDesign3());
+//         }).start();
+//         new Thread(() -> {
+//         System.out.println(SignalDesign3.getSignalDesign3());
+//         }).start();
+//         new Thread(() -> {
+//         System.out.println(SignalDesign3.getSignalDesign3());
+//         }).start();
+//         new Thread(() -> {
+//         System.out.println(SignalDesign3.getSignalDesign3());
+//         }).start();
